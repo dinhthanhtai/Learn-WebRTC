937a43a9122583fe50f5565a59c3a1af6f1fa3e7
GwynDinh <dinhthanhtai.xz@gmail.com>
2023-04-24 12:27:45 +0700

    handle multiple devices

--- main.js
+++ main.js
@@ -1,45 +1,38 @@
-function hasUserMedia () {
-    return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia.mozGetUserMedia || navigator.webkitGetUserMedia || navigator.msGeneric || navigator.mozGetUserMedia || navigator.webkitGeneric || navigator.msGetUserMedia) 
-}
-
-var constraints = {
-    video: {
-        mandatory: {
-            minWith: 640,
-            minHeight: 480,
-        }
-    },
-    audio: true
-}
-
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera mini/i.test(navigator.userAgent)) {
-    console.log('true ne')
-    constraints = {
-        video: {
-            mandatory: {
-                minWith: 480,
-                minHeight: 320,
-                maxWidth: 1024,
-                maxHeight: 768
-            }
-        },
-        audio: true
-    }
-}
-
-if (hasUserMedia()) {
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.webkitGeneric;
-
-    navigator.getUserMedia(constraints, function (stream) {
-        console.log("🚀 ~ file: main.js:9 ~ stream:", stream)
-        var video = document.querySelector('video');
-        const videoTracks = stream.getVideoTracks();
-        console.log("🚀 ~ file: main.js:25 ~ videoTracks:", videoTracks)
-        console.log(`Using video device: ${videoTracks[0].label}`);
-        window.stream = stream; // make variable available to browser console
-        video.srcObject = stream;
-    }, function (err) {})
-} else {
-    alert('Sorry, your browser does not support getUserMedia');
-}
-
+if (!navigator.mediaDevices?.enumerateDevices) {
+    console.log("enumerateDevices() not supported.");
+  } else {
+      navigator.mediaDevices.enumerateDevices().then(function(sources) {
+          var audioSource = null;
+          var videoSource = null;
+      
+          for (var i = 0; i < sources.length; i++) {
+              var source = sources[i];
+              if (source.kind === 'audioinput') {
+                  console.log('Microphone found:', source.label, source.deviceId);
+                  audioSource = source.deviceId;
+              } else if (source.kind === 'videoinput') {
+                  console.log('Camera found:', source.label, source.deviceId)
+                  videoSource = source.deviceId;
+              } else {
+                  console.log('Unknown source:', source);
+              }
+          }
+      
+          var constraints = {
+              audio: {
+                  optional: [{sourceId: audioSource}]
+              },
+              video: {
+                  optional: [{sourceId: videoSource}]
+              }
+          }
+      
+          navigator.getUserMedia(constraints, function(stream) {
+              var video = document.querySelector('video');
+              window.stream = stream;
+              video.srcObject = stream;
+          }, function(error) {
+              console.log('Raised an error when capturing:', error);
+          })
+      }) 
+  }
